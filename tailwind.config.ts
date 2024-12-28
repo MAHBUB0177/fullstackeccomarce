ef0aa7a47ca5ddedca32a36696385feb26a06b30
@mahbub.alam <mahbub.alam@technonext.com>
2024-12-28 07:34:14 +0600

    updte package.loc

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -43,3 +43,7 @@ const config: Config = {
   plugins: [],
 };
 export default config;
+      // "antd": "^5.18.1",
+       // "next": "14.2.4",
+    // "react": "^18.2.0",
+    // "react-dom": "^18.2.0",
